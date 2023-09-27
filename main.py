b96b647ee95182bc64e043fe7ed419ae4bb44a2c
baileystarr4 <baileystarrc4@gmail.com>
2023-09-27 19:22:41 -0400

    Find the code link

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -18,12 +19,25 @@ try:
     )
     skip_button.click()
 except:
-    driver.quit()
+    print("NOT FOUND 1")
 try:
     newest_article = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'code')]"))    
     )
     newest_article.click()
 except:
-    print("NOT FOUND")
-#driver.quit()
+    print("NOT FOUND 2")
+# try:
+#     code = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.CSS_SELECTOR, "a[href^='https://genshin.hoyoverse.com/en/gift']"))    
+#     )
+#     code.click()
+# except:
+#     print("NOT FOUND 3")
+try:
+    code = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href^='https://genshin.hoyoverse.com/en/gift']"))    
+    )
+    code.click()
+except:
+    print("NOT FOUND 4")
