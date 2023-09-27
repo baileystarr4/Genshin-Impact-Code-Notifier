42eefa8c0a59b3c26c7c6aeb0920d41c77854961
baileystarr4 <baileystarrc4@gmail.com>
2023-09-27 17:33:22 -0400

    Automate clicking the skip button
    Automate clicking the skip button on the popup that comes up when the page is first loaded

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -9,8 +11,13 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.hoyolab.com/creatorCollection/535336")
 
 # driver.maximize_window()
+# time.sleep(10)
+try:
+    skip_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[3]/div/div/div/div[1]/div/button'))    
+    )
+    skip_button.click()
+except:
+    driver.quit()
 
-# skip_button = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div/div/div[1]/div/button")
-# skip_button.click()
-# codes = driver.find_element(By.CSS_SELECTOR, "title*='code'")
-# print(codes)
+#driver.quit()
