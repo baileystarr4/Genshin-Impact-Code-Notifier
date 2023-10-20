7bb8ab3f3b2c175eb09c9ee9a113eb25ddf2d64c
baileystarr4 <baileystarrc4@gmail.com>
2023-10-20 16:48:10 -0400

    Editing and Adding clicks for new website pop ups
    Editing code comments and error messages for readability. Also added script to click the close button on the new log in button that hoyoverse added

--- main.py
+++ main.py
@@ -9,35 +9,51 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
+# Open Brutefact's Redeem Codes and Web Events collection page on the Hoyolab website
 driver.get("https://www.hoyolab.com/creatorCollection/535336")
 
-# driver.maximize_window()
-# time.sleep(10)
+# Look for the iframe where the login popup is located
 try:
+    iframe = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'hyv-account-frame'))
+    )
+    driver.switch_to.frame(iframe)
+except:
+    print("Frame not found")
+
+# Look for the close button on the login pop-up and click when found.
+try:
+    close_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div/button'))    
+    )
+    close_button.click()
+except:
+    print("Close button not found")
+
+# Look for skip button on pop-up and click when found.
+try:
+    driver.switch_to.default_content()
     skip_button = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="__layout"]/div/div[3]/div/div/div/div[1]/div/button'))    
     )
     skip_button.click()
 except:
-    print("NOT FOUND 1")
+    print("Skip button not found")
+
+# Look for newest article mentioning a code and click when found.    
 try:
     newest_article = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'code')]"))    
     )
     newest_article.click()
 except:
-    print("NOT FOUND 2")
-# try:
-#     code = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.CSS_SELECTOR, "a[href^='https://genshin.hoyoverse.com/en/gift']"))    
-#     )
-#     code.click()
-# except:
-#     print("NOT FOUND 3")
+    print("Code article not found")
+
+# Look for the code link and click when found.
 try:
     code = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href^='https://genshin.hoyoverse.com/en/gift']"))    
     )
     code.click()
 except:
-    print("NOT FOUND 4")
+    print("Code link not found")
