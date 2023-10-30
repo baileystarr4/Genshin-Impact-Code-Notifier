9abf5809c6f8d989526e1220748f6d755a2da313
baileystarr4 <baileystarrc4@gmail.com>
2023-10-30 18:18:18 -0400

    Added unsubscribe page and function

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function unsubscribe(userId) {
-  fetch("/unsubscribe", {
-    method: "POST",
-    body: JSON.stringify({ userId: userId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
--- website/views.py
+++ website/views.py
@@ -31,14 +31,21 @@ def home():
 
     return render_template("home.html")
 
+@views.route('/unsubscribe', methods=['GET', 'POST'])
+def unsubscribe():
+    if request.method == 'POST':
+        phone_number = request.form.get('phone_number')
+        sure_check = request.form.get('sure_check')
+        user = User.query.filter_by(phone_number=phone_number).first()
+        if len(phone_number) < 9:
+            flash('Phone number must be 9 digits.', category='error')
+        elif not sure_check:
+            flash('Please check the "are you sure box".', category='error')
+        elif not user:
+            flash('This phone number does not exist in our database. Please try again', category='error')
+        else:
+            db.session.delete(user)
+            db.session.commit()
+            flash('Success. You will no longer receive text notifications.', category='success')
 
-@views.route('/unsubscribe', methods=['POST'])
-def unsubscribe():  
-    user = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    userId = user['id']
-    user = User.query.get(userId)
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-
-    return jsonify({})
+    return render_template('unsubscribe.html')
