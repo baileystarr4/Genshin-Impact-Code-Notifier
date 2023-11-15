e473469c3c892008fbdc407ffe01a79bd6274458
baileystarr4 <baileystarrc4@gmail.com>
2023-11-15 15:08:24 -0500

    Delete database to move to spreadsheets
    Changing the website to use a google spreadsheet instead of a data base. deleting unnecessary files and commenting out code that needs changed after spreadsheet class is implemented.

--- website/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-from sqlalchemy.sql import func
-
-
-class User(db.Model):
-    phone_number = db.Column(db.String(9), primary_key=True)
-    first_name = db.Column(db.String(25))
-    carrier = db.Column(db.String(25))
