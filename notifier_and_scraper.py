1107f460e3ab1afa5de6c18f58e774e943dd9a8f
baileystarr4 <baileystarrc4@gmail.com>
2023-11-16 14:35:30 -0500

    Update the script to add the found links to the spread sheetif they're not already in the sheet

--- notifier_and_scraper.py
+++ notifier_and_scraper.py
@@ -1,14 +1,14 @@
 from WebScraper import *
-# from Notifier import *
+from Notifier import *
 from Spreadsheet import *
 
-# web_scraper = WebScraper()
+web_scraper = WebScraper()
+links = web_scraper.find_links()
 
-# link = web_scraper.find_code_and_link()
-
-# notifier = Notifier()
-# notifier.send_sms_via_email(link=link, number="NUMBER", provider="AT&T")
-
-# spreadsheet_brain = Spreadsheet()
-# spreadsheet_brain.write_to_code_spreadsheet(link)
-# spreadsheet_brain.write_to_user_data_spreadsheet([2343,"Bob", "AT&T"])
+spreadsheet = Spreadsheet()
+saved_codes = spreadsheet.read_code_spreadsheet()
+ 
+last_code = len(saved_codes) - 1
+if links[0] != saved_codes[last_code]:
+    for link in links:
+        spreadsheet.write_to_code_spreadsheet([link, "not sent"])
