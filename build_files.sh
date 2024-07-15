91df7f9c16c4f3d0347b79dfb9a9ed40afe38b1e
baileystarr4 <baileystarrc4@gmail.com>
2024-07-15 15:57:11 -0400

    Revert "13"
    This reverts commit 82096d5271d45cc8727e133b74bb5531167d736a.

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,18 @@
+echo "BUILD START"
+
 # create a virtual environment named 'venv' if it doesn't already exist
-python -m venv venv
+python3.9 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
 
-python pip install -U pip
-
 # install all deps in the venv
 pip3 install -r requirements.txt
-
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
 
 # collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
 
+echo "BUILD END"
 
 # [optional] Start the application here 
 python manage.py runserver
