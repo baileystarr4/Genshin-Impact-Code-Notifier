ba4f666a9cd18b50e2c4c6cb5b05117970ab5fb2
baileystarr4 <baileystarrc4@gmail.com>
2024-07-15 16:36:23 -0400

    21

--- website/website/urls.py
+++ website/website/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("genshin_code_notifier.urls"))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
