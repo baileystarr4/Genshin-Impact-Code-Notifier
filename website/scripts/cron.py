55ff46fa195e0ca6725177fd6561767f9185e249
baileystarr4 <baileystarrc4@gmail.com>
2024-07-16 18:08:26 -0400

    create handler class

--- website/scripts/cron.py
+++ website/scripts/cron.py
@@ -7,29 +7,33 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 django.setup()
 from genshin_code_notifier.models import Code
 
-def run():
-    """
-    Run the daily task of scraping for new links, notifying subscribers if found, 
-    and deleting old links from the database.
-    """
+class handler():
 
-    web_scraper = WebScraper()
-    links = web_scraper.find_links()
+    def run(self):
+        """
+        Run the daily task of scraping for new links, notifying subscribers if found, 
+        and deleting old links from the database.
+        """
 
-    # Continue only if the web scraper was successful.
-    if links:
-        try:
-            # Is the code link already in the database?
-            Code.objects.get(link=links[0])
+        web_scraper = WebScraper()
+        links = web_scraper.find_links()
 
-        except Code.DoesNotExist:
-            notifier = Notifier()
-            notifier.send_links(links)
+        # Continue only if the web scraper was successful.
+        if links:
+            try:
+                # Is the code link already in the database?
+                Code.objects.get(link=links[0])
 
-            for link in links:
-                new_code = Code(link = link)
-                new_code.save()
+            except Code.DoesNotExist:
+                notifier = Notifier()
+                notifier.send_links(links)
 
-    # Delete any link older than 60 days.
-    thirty_days_ago = timezone.now() - timedelta(days=60)
-    Code.objects.filter(date__lte=thirty_days_ago).delete()
+                for link in links:
+                    new_code = Code(link = link)
+                    new_code.save()
+
+        # Delete any link older than 60 days.
+        thirty_days_ago = timezone.now() - timedelta(days=60)
+        Code.objects.filter(date__lte=thirty_days_ago).delete()
+
+    
