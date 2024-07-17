bb3988f9f294a22a618a06339554fc8f324500e7
baileystarr4 <baileystarrc4@gmail.com>
2024-07-17 13:32:48 -0400

    Add selenium storage

--- website/scripts/webscraper.py
+++ website/scripts/webscraper.py
@@ -21,7 +21,8 @@ class WebScraper:
 
         self.chrome_options.add_argument('headless')
         self.chrome_options.add_argument("--log-level=3")
-        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(driver_version="114.0.5735.90").install()),options=self.chrome_options)
+        self.storage = { 'request_storage_base_dir': '/tmp' }
+        self.driver = webdriver.Chrome(seleniumwire_options=self.storage, service=ChromeService(ChromeDriverManager(driver_version="114.0.5735.90").install()),options=self.chrome_options)
         self.error_notifier = Notifier()
         
         self.links = []
