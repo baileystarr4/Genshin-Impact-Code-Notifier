761409877dfc811ca3198b1b277c56fbabc90196
Bailey Crumpler <baileystarrc4@gmail.com>
2025-07-23 14:15:03 -0400

    Rename scheduled task files

--- website/cron.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\Bailey\Documents\GitHub\Genshin-Notification-Website
-call .venv\Scripts\activate
-cd website
-python manage.py runscript cron
---
+++ website/scheduled_task.bat
@@ -0,0 +1,4 @@
+cd C:\Users\user\project
+call .venv\Scripts\activate
+cd website
+python manage.py runscript scheduled_task
